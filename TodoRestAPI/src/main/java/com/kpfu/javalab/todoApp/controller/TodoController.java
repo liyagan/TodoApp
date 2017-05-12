f93eb3e4c9ecd3559fd244a2cdfebc7dd6d0bba3
liyagan <liya-ganeyeva39@mail.ru>
2017-05-12 12:47:56 +0300

    Initial commit

---
+++ TodoRestAPI/src/main/java/com/kpfu/javalab/todoApp/controller/TodoController.java
@@ -0,0 +1,75 @@
+package com.kpfu.javalab.todoApp.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kpfu.javalab.todoApp.entity.Todo;
+import com.kpfu.javalab.todoApp.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+
+/**
+ * Created by Лия on 03.05.2017.
+ */
+@RestController
+public class TodoController {
+
+    @Autowired
+    TodoService todoService;
+
+    final ObjectMapper mapper = new ObjectMapper();
+    final StringWriter sw = new StringWriter();
+
+    @RequestMapping(value = "/todos", method = RequestMethod.GET)
+    public List<String> getAllTodos() {
+        List<Todo> todos = todoService.getAllTodos();
+        try {
+            mapper.writeValue(sw, todos);
+            System.out.println(sw.toString());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/api/todo", method = RequestMethod.POST)
+    @ResponseBody
+    public Todo add(@Valid @RequestBody Todo todo) {
+        return todoService.add(todo);
+    }
+
+    @RequestMapping(value = "/api/todo/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public void deleteById(@PathVariable("id") Long id) {
+
+        todoService.deleteById(id);
+    }
+
+    @RequestMapping(value = "/api/todo", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Todo> findAll() {
+
+        return todoService.findAll();
+    }
+
+
+    @RequestMapping(value = "/api/todo/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Todo findById(@PathVariable("id") Long id) {
+
+        return todoService.findById(id);
+    }
+
+    @RequestMapping(value = "/api/todo/{id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public Todo update(@Valid @RequestBody Todo todo, @PathVariable("id") Long todoId) {
+        return todoService.update(todo);
+    }
+
+}
